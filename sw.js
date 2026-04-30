6a97515aee41f50e28b7e967d2d5441acdff8add
raed9-git <raedhamdyy@gmail.com>
2026-04-30 08:15:40 +0300

    Update sw.js

---
+++ sw.js
@@ -0,0 +1,12 @@
+{
+  "keys": [
+    {
+      "kty": "RSA",
+      "use": "sig",
+      "kid": "gym-app-key-id",
+      "n": "sample-key-data-placeholder",
+      "e": "AQAB",
+      "alg": "RS256"
+    }
+  ]
+}
